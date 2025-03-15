b6f4d7cfdf231aacfba5a69031523de3f7bfa101
Abstract Programmer <AbstractProgrammer@example.com>
2025-03-15 05:07:17 +0100

    new

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'root', // Replace with your PostgreSQL username
-  host: 'oregon-postgres.render.com', // PostgreSQL host on Render
-  database: 'expire_db_qrqg', // Replace with your PostgreSQL database name
-  password: 'JEe1piCBZMcqtNMs9xCgi51uYArDHSRt', // Replace with your PostgreSQL password
+  host: 'dpg-cvaejlnnoe9s73f83ed0-a.oregon-postgres.render.com', // PostgreSQL host on Render
+  database: 'moto_db_mdly', // Replace with your PostgreSQL database name
+  password: 'ZmBlC74h6uCW9P77PgSHzoYdIJQQYLIc', // Replace with your PostgreSQL password
   port: 5432, // PostgreSQL port
   ssl: {
     rejectUnauthorized: false, // This allows SSL without verifying the certificate
