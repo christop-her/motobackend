5f783ae8f4b7c4f889b642697d13aa871d15aafc
Abstract Programmer <AbstractProgrammer@example.com>
2025-03-15 05:00:09 +0100

    new

--- authController.js
+++ authController.js
@@ -1,11 +1,11 @@
 // const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const crypto = require("crypto");
-const nodemailer = require("nodemailer");
+// const crypto = require("crypto");
+// const nodemailer = require("nodemailer");
 const pooll = require('./db');
 // require('.env').config();
-const multer = require("multer");
-const path = require("path");
+// const multer = require("multer");
+// const path = require("path");
 
 
 
